c3f763b3cde631ad94e6d483135e808f0cdea0f7
NEDNAR <drakozavrov20@gmail.com>
2021-06-02 16:02:06 +0300

    Compilation errors totally fixed

--- Assets/!Code/Tweening/MainWindowView.cs
+++ Assets/!Code/Tweening/MainWindowView.cs
@@ -6,30 +6,30 @@ using UnityEngine.UI;
 
 namespace Tweening
 {
-    public class MainWindowView : MonoBehaviour
-    {
-        [SerializeField] private Button _openPopupButton;
-        [SerializeField] private PopupView _popupView;
-
-        [SerializeField] private Button _changeTextButton;
-        [SerializeField] private Text _changeText;
-
-        private void Start()
-        {
-            _openPopupButton.onClick.AddListener(_popupView.ShowPopup);
-            _changeTextButton.onClick.AddListener(ChangeText);
-        }
-
-        private void OnDestroy()
-        {
-            _openPopupButton.onClick.RemoveAllListeners();
-            _changeTextButton.onClick.RemoveAllListeners();
-            _changeText.DOKill();
-        }
-
-        private void ChangeText()
-        {
-            _changeText.DOText("New text", 1.0f).SetEase(Ease.Linear);
-        }
-    }
+    // public class MainWindowView : MonoBehaviour
+    // {
+    //     [SerializeField] private Button _openPopupButton;
+    //     [SerializeField] private PopupView _popupView;
+    //
+    //     [SerializeField] private Button _changeTextButton;
+    //     [SerializeField] private Text _changeText;
+    //
+    //     private void Start()
+    //     {
+    //         _openPopupButton.onClick.AddListener(_popupView.ShowPopup);
+    //         _changeTextButton.onClick.AddListener(ChangeText);
+    //     }
+    //
+    //     private void OnDestroy()
+    //     {
+    //         _openPopupButton.onClick.RemoveAllListeners();
+    //         _changeTextButton.onClick.RemoveAllListeners();
+    //         _changeText.DOKill();
+    //     }
+    //
+    //     private void ChangeText()
+    //     {
+    //         _changeText.DOText("New text", 1.0f).SetEase(Ease.Linear);
+    //     }
+    // }
 }
