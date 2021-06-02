50187a75259ab7741d8b2aa20bb8093043b9c5da
NEDNAR <drakozavrov20@gmail.com>
2021-06-02 17:59:00 +0300

    Working addressables

--- Assets/!Code/Bundles/LoadWindowView.cs
+++ Assets/!Code/Bundles/LoadWindowView.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 
 
@@ -7,16 +10,30 @@ namespace Bundles
 {
     public class LoadWindowView : AssetBundleViewBase
     {
+        [SerializeField] private AssetReference _loadPrefab;
+        [SerializeField] private RectTransform _mountSpawnTransform;
         [SerializeField] private Button _loadAssetsButton;
+        [SerializeField] private Button _spawnAssetsButton;
+
+        private List<AsyncOperationHandle<GameObject>> _addressablePrefabs =
+            new List<AsyncOperationHandle<GameObject>>();
 
         private void Start()
         {
             _loadAssetsButton.onClick.AddListener(LoadAsset);
+            _spawnAssetsButton.onClick.AddListener(CreateAddressablesPrefab);
         }
 
         private void OnDestroy()
         {
             _loadAssetsButton.onClick.RemoveAllListeners();
+            
+            foreach (var addressablePrefab in _addressablePrefabs)
+            {
+                Addressables.ReleaseInstance(addressablePrefab);
+            }
+            
+            _addressablePrefabs.Clear();
         }
 
         private void LoadAsset()
@@ -24,6 +41,12 @@ namespace Bundles
             _loadAssetsButton.interactable = false;
 
             StartCoroutine(DownloadAndSetAssetBundle());
+        }
+
+        private void CreateAddressablesPrefab()
+        {
+            var addressablePrefab = Addressables.InstantiateAsync(_loadPrefab, _mountSpawnTransform);
+            _addressablePrefabs.Add(addressablePrefab);
         }
     }
 }
